aff060486c10c179f226f98a8bb644bd310d6a90
Paweł J <jpawz@outlook.com>
2023-01-10 19:17:50 +0100

    Add form to generate grid

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-for (let row = 0; row < 16; row++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.style.cssText = "display: flex; flex-direction: row;";
-  document.body.appendChild(rowDiv);
-  for (let column = 0; column < 16; column++) {
-    const div = document.createElement("div");
-    div.style.cssText = "width: 50px; height: 50px; background-color: gray;";
-    div.className = "cell";
-    rowDiv.appendChild(div);
+const grid = document.getElementById("grid");
+
+function generateGrid(squares, cellSize) {
+  for (let row = 0; row < squares; row++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.style.cssText = "display: flex; flex-direction: row;";
+    grid.appendChild(rowDiv);
+    for (let column = 0; column < squares; column++) {
+      const div = document.createElement("div");
+      div.style.cssText = `width: ${cellSize}px; height: ${cellSize}px; background-color: gray;`;
+      div.className = "cell";
+      rowDiv.appendChild(div);
+    }
   }
+
+  document.querySelectorAll(".cell").forEach((cell) =>
+    cell.addEventListener("mouseenter", (event) => {
+      event.target.style.cssText = `width: ${cellSize}px; height: ${cellSize}px; background-color: black;`;
+    })
+  );
 }
 
-document.querySelectorAll(".cell").forEach((cell) =>
-  cell.addEventListener("mouseenter", (event) => {
-    event.target.style.cssText =
-      "width: 50px; height: 50px; background-color: black;";
-    console.log(event);
-  })
-);
+function clearGrid() {
+  grid.replaceChildren();
+}
+
+const generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", () => {
+  const squares = document.getElementById("squares").value;
+  const cellSize = document.getElementById("cellSize").value;
+  clearGrid();
+  generateGrid(squares, cellSize);
+});
